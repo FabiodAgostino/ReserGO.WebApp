3aab91f1091b0a16c3b87d01dfdd528c7687ef5a
Daniele Fastelli <daniele.fastelli@descor.com>
2024-09-30 17:43:39 +0200

    #fix resize

--- ReserGO.ViewModel/Extensions/VMCollectionExtensions.cs
+++ ReserGO.ViewModel/Extensions/VMCollectionExtensions.cs
@@ -3,12 +3,14 @@ using ReserGO.ViewModel.Interface.Authentication;
 using ReserGO.ViewModel.Interface.FiltersComponent;
 using ReserGO.ViewModel.Interface.Header;
 using ReserGO.ViewModel.Interface.Home;
+using ReserGO.ViewModel.Interface.LoginHome;
 using ReserGO.ViewModel.Interface.Register;
 using ReserGO.ViewModel.Interface.Utils;
 using ReserGO.ViewModel.ViewModel.Authentication;
 using ReserGO.ViewModel.ViewModel.FiltersComponent;
 using ReserGO.ViewModel.ViewModel.Header;
 using ReserGO.ViewModel.ViewModel.Home;
+using ReserGO.ViewModel.ViewModel.LoginHome;
 using ReserGO.ViewModel.ViewModel.Register;
 using ReserGO.ViewModel.ViewModel.Utils;
 
@@ -24,6 +26,7 @@ namespace ReserGO.ViewModel.Extensions
             services.AddScoped<IHeaderViewModel, HeaderViewModel>();
             services.AddTransient<IRegisterViewModel, RegisterViewModel>();
             services.AddScoped<IComuneComboViewModel, ComuneComboViewModel>();
+            services.AddScoped<ILoginHomeViewModel, LoginHomeViewModel>();
 
 
             return services;
