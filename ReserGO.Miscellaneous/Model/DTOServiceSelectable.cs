341a0e8b4df323db4e8c559c4ab1e9d9cd621c0f
Daniele Fastelli <daniele.fastelli@descor.com>
2024-10-21 18:09:26 +0200

    implementazione services

---
+++ ReserGO.Miscellaneous/Model/DTOServiceSelectable.cs
@@ -0,0 +1,15 @@
+﻿using ReserGO.DTO;
+
+namespace ReserGO.Miscellaneous.Model
+{
+    public class DTOServiceSelectable
+    {
+        public bool IsSelected { get; set; }
+        public DTOService Service { get; set; }
+        public DTOServiceSelectable(DTOService service)
+        {
+            Service = service;
+        }
+
+    }
+}
